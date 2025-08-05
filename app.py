abda6a61d833b66598df7be5463257e684053cb2
SOUMYA RANJAN PATRA <1103775@shyamgroup.com>
2025-08-05 16:45:54 +0530

    add os

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for
+import os
 
 app = Flask(__name__)
 
@@ -48,4 +49,14 @@ def contact():
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+
+
+
+
+
+
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
